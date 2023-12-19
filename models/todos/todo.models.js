acfa97466b069f935bf62000d5d3ee879bcc4327
shadoow9233 <bibekkunwar66632@gmail.com>
2023-12-20 05:37:02 +0545

    Todos Data modeling update

---
+++ models/todos/todo.models.js
@@ -0,0 +1,25 @@
+
+import mongoose from "mongoose";
+const todoSchema=new mongoose.Schema(
+    {
+     content:{
+        type:String,
+        require:time,
+
+     } ,
+     createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+     },
+     SubTodo:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"SubTodo"
+        }
+     ]
+
+    },{timestamps:true}
+  
+
+)
+export const Todo=mongoose.model("Todo",todoSchema)
